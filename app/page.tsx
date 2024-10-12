b7e82c230c9b9b8c48fa67045b5fd96cd57516bc
vedanthv-cc <vedanth.v@codecraft.co.in>
2024-10-12 16:19:48 +0530

    updated landing page content

--- app/page.tsx
+++ app/page.tsx
@@ -15,13 +15,16 @@ const Home: React.FC = () => {
   };
 
   return (
-    <main className="w-full h-screen bg-black flex flex-col items-center justify-center">
+    <main className="w-full h-screen bg-gradient-to-r from-gray-800 to-gray-900 flex flex-col items-center justify-center">
       <div className="w-full h-full flex flex-col items-center justify-center p-4">
         <Terminal />
         {/* Portfolio button */}
+        <p className="text-white text-lg mb-4 text-center">
+          If you are not a CS person, don&apos;t worry! 😅👇
+        </p>
         <button
           onClick={handleRedirect}
-          className="mt-6 bg-green-500 text-black font-bold py-2 px-4 rounded-md hover:bg-green-600 transition duration-300"
+          className="mt-6 bg-green-500 text-white font-bold py-2 px-6 rounded-md shadow-lg hover:bg-green-600 transition duration-300 transform hover:scale-105"
         >
           View Portfolio
         </button>
